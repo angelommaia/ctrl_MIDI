6d2c3cd0687522cb82559af11b28319506d1abb0
Angelo <angelomaia@id.uff.br>
2020-01-14 08:46:47 -0300

    Botao basico

--- botoes.h
+++ botoes.h
@@ -1,23 +1,16 @@
-
 #ifndef botoes_h
 #define botoes_h
-
 #include "Arduino.h"
-
-
-class Botao{
-
-public:
-	Botao(int pin, int note, int led);
-	void estado();
-	void ativo();
-	void inativo();
-private:
-	int _pin;
-	int _note;
-	int _led;
-	int _nivel=LOW;
-};
-
-
+  class Botao{
+    public:
+    	Botao(int pin, int note, int led);
+    	void estado();
+    	void ativo();
+    	void inativo();
+    private:
+    	int _pin;
+    	int _note;
+    	int _led;
+    	int _nivel=LOW;
+  };
 #endif
--- ctrl_midi.ino
+++ ctrl_midi.ino
@@ -1,16 +1,12 @@
- #include "botoes.h"
+#include "botoes.h"
+Botao botao(2,61,13);
 
- Botao botao(2,61,13);
-  
-  void setup() {  
-    Serial.begin(115200); //o numero deve ser o baud configurado no hairless
-    //portas
-  }
-  
-  void loop() {  
-    //rotina botao 1 
-    botao.estado();
+void setup() {  
+Serial.begin(115200); //o numero deve ser o baud configurado no hairless
+}
 
-  }
+void loop() {  
+botao.estado();
+}
 
     
